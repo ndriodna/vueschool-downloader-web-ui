b495585b369557326572fdad2d4fb9470275c235
ndriodna <kurodesu6969@gmail.com>
2024-04-30 16:40:46 +0800

    add: selected courses

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,43 @@
-<script setup></script>
+<script setup>
+import { watchEffect } from 'vue';
+
+const emit = defineEmits(['checked'])
+
+</script>
 <template>
-    <div class="w-72 relative">
-        <label class="cursor-pointer">
-            <input type="checkbox" class="peer sr-only">
-            <span class="opacity-100 peer-checked:opacity-0 absolute right-2 top-2 z-10 transition-all ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
-                    style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-                    <path
-                        d="M12 2C6.486 2 2 6.486 2 12c.001 5.515 4.487 10.001 10 10.001 5.514 0 10-4.486 10.001-10.001 0-5.514-4.486-10-10.001-10zm0 18.001c-4.41 0-7.999-3.589-8-8.001 0-4.411 3.589-8 8-8 4.412 0 8.001 3.589 8.001 8-.001 4.412-3.59 8.001-8.001 8.001z">
-                    </path>
-                </svg>
-            </span>
-            <span class="opacity-0 peer-checked:opacity-100 absolute right-2 top-2 z-10">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
-                    style="fill: rgba(22, 163, 74, 1);transform: ;msFilter:;">
-                    <path
-                        d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-1.999 14.413-3.713-3.705L7.7 11.292l2.299 2.295 5.294-5.294 1.414 1.414-6.706 6.706z">
-                    </path>
-                </svg>
-            </span>
-            <div
-                class="transition-all active:scale-90 flex flex-col space-y-4 justify-center rounded-lg bg-white px-8 py-8 ring ring-transparent peer-checked:ring-green-500">
-                <div class="flex justify-start">
-                    <div class="text-red-600 font-mono text-lg uppercase tracking-wide">Premium</div>
-                </div>
-                <div class=" bg-slate-500 h-40 overflow-hidden rounded-xl ">
-                    <img src="https://picsum.photos/200/300" alt="" class="rounded-md w-full bg-contain">
-                </div>
-                <div class="font-medium text-lg text-justify tracking-tight">Lorem ipsum dolor sit amet, consectetur
-                    adipisicing
-                    elit.</div>
-                <div
-                    class="p-1 bg-[#736CC8] flex justify-between space-x-6 rounded-xl font-bold mb-12 px-2 items-center">
-                    <div class="text-white">10 lesson</div>
-                    <div class="text-green-400">Beginner</div>
-                </div>
+    <label class="cursor-pointer">
+        <input type="checkbox" @change="$emit('checked', $event.target)" class="peer sr-only">
+        <span class="opacity-100 peer-checked:opacity-0 absolute right-2 top-2 z-10 transition-all ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                style="fill: rgba(0, 0, 0, 1);">
+                <path
+                    d="M12 2C6.486 2 2 6.486 2 12c.001 5.515 4.487 10.001 10 10.001 5.514 0 10-4.486 10.001-10.001 0-5.514-4.486-10-10.001-10zm0 18.001c-4.41 0-7.999-3.589-8-8.001 0-4.411 3.589-8 8-8 4.412 0 8.001 3.589 8.001 8-.001 4.412-3.59 8.001-8.001 8.001z">
+                </path>
+            </svg>
+        </span>
+        <span class="opacity-0 peer-checked:opacity-100 absolute right-2 top-2 z-10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                style="fill: rgba(22, 163, 74, 1);">
+                <path
+                    d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-1.999 14.413-3.713-3.705L7.7 11.292l2.299 2.295 5.294-5.294 1.414 1.414-6.706 6.706z">
+                </path>
+            </svg>
+        </span>
+        <div
+            class="transition-all active:scale-90 flex flex-col space-y-4 justify-center rounded-lg bg-white px-8 py-8 ring ring-transparent peer-checked:ring-green-500">
+            <div class="flex justify-start">
+                <div class="text-red-600 font-mono text-lg uppercase tracking-wide">Premium</div>
             </div>
-        </label>
-    </div>
+            <div class=" bg-slate-500 h-40 overflow-hidden rounded-xl ">
+                <img src="https://picsum.photos/200/300" alt="" class="rounded-md w-full bg-contain">
+            </div>
+            <div class="font-medium text-lg text-justify tracking-tight">Lorem ipsum dolor sit amet, consectetur
+                adipisicing
+                elit.</div>
+            <div class="p-1 bg-[#736CC8] flex justify-between space-x-6 rounded-xl font-bold mb-12 px-2 items-center">
+                <div class="text-white">10 lesson</div>
+                <div class="text-green-400">Beginner</div>
+            </div>
+        </div>
+    </label>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <script setup>
+import { ref, watch, watchEffect } from 'vue';
 import Card from '../components/Card.vue';
 import Filter from '../components/Filter.vue';
+
+const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+const selectedCourse = ref()
+
+const courses = ref([])
+function selectCourse(id) {
+
+  if (courses.value.includes(id)) {
+    courses.value.splice(courses.value.indexOf(id), 1)
+  } else {
+    courses.value.push(id)
+  }
+}
 
 </script>
 
@@ -8,17 +23,10 @@ import Filter from '../components/Filter.vue';
   <main class="px-12">
     <Filter />
 
-    <div class="flex flex-auto flex-wrap justify-between py-2 my-2">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    {{ courses }}
+    <div class="flex flex-auto flex-wrap justify-between py-2 gap-4">
+      <Card v-for="(d, index) in data" :key="index" class="w-72 relative" @checked="selectCourse(index)"
+        v-model="selectedCourse" />
     </div>
   </main>
 </template>

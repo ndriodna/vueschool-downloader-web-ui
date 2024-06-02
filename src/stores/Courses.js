85e143bc35059c457f07652177c04ea20e92c574
ndriodna <kurodesu6969@gmail.com>
2024-06-03 03:12:07 +0800

    fix: ref issue multi update object change ref to reactivity

--- src/stores/Courses.js
+++ src/stores/Courses.js
@@ -1,9 +1,9 @@
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import { defineStore } from "pinia";
 
 export const useCoursesStore = defineStore("courses", () => {
-  const selected = ref([]);
-  const datas = ref([
+  const selected = reactive([]);
+  const datas = reactive([
     {
       id: 1783456213,
       title: "JavaScript for Beginners",
@@ -85,9 +85,37 @@ export const useCoursesStore = defineStore("courses", () => {
       level: "beginner",
     },
   ]);
-  function selectCourses() {
-    count.value++;
+
+  function isChecked(event) {
+    datas.map((val) =>
+      val.id === event.id
+        ? { ...val, isSelected: (val.isSelected = !event.isSelected) }
+        : val
+    );
   }
 
-  return { selected, datas, selectCourses };
+  function clearSelected() {
+    selected.length = 0;
+    datas.map((data) => (data.isSelected = false));
+  }
+
+  function selectCourses(event) {
+    if (selected.includes(event.id)) {
+      selected.splice(selected.indexOf(event.id), 1);
+    } else {
+      selected.push(event.id);
+    }
+    isChecked(event);
+  }
+  function selectedCourses() {
+    return datas.filter((data) => selected.includes(data.id));
+  }
+  return {
+    selected,
+    datas,
+    isChecked,
+    clearSelected,
+    selectCourses,
+    selectedCourses,
+  };
 });
